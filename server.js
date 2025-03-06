7ccb026093c41ddda8375cc5053db176fb0c86e9
dikshantadlakha <dikshant1234@yahoo.com>
2025-03-06 13:12:15 +0530

    Finalized changes - updated header and navbar

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+<<<<<<< HEAD
 // Serve static files from the public directory
 app.use(express.static('views/public'));
+=======
+// Serve static files correctly from the "public" directory
+app.use(express.static('public'));
+>>>>>>> 336cf86 (Initial commit - Node.js EJS project)
 
 // Set EJS as the templating engine
 app.set("view engine", "ejs");
 
 // Define a route
 app.get("/", (req, res) => {
+<<<<<<< HEAD
   res.render("index", { title: "My EJS Page", message: "Hello, EJS!" });
+=======
+  res.render("index");
+>>>>>>> 336cf86 (Initial commit - Node.js EJS project)
 });
 
 // Start the server
